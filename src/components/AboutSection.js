47c8e0998b1ca43685e226dcccfb578ad4251e51
nicksharma0 <sharma.nick67@gmail.com>
2025-07-04 20:45:39 -0500

    Fix profile picture path to work across all routes

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -6,7 +6,7 @@ const AboutSection = ({ darkMode }) => (
     <h2 className="about-title">About the Creator</h2>
     <div className="about-content">
       <div className="profile-pic-wrapper">
-        <img className="profile-pic" src="./profile-placeholder.jpg" alt="Nick Sharma profile" />
+        <img className="profile-pic" src={`${process.env.PUBLIC_URL}/profile-placeholder.jpg`} alt="Nick Sharma profile" />
       </div>
       <p>
         "I was fortunate enough to have amazing math teachers in high school, but I know that not everyone has that experience."
